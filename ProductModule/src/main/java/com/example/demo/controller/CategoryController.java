2bfc5d67144671aa5f933a042c2185367bb8710b
xshrfnd <39291597+shrfnd123@users.noreply.github.com>
2023-07-07 13:42:35 +0800

    Fixed CRUD functions and return data into json type

--- ProductModule/src/main/java/com/example/demo/controller/CategoryController.java
+++ ProductModule/src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,13 +1,12 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 
 import com.example.demo.entity.Category;
@@ -23,15 +22,42 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @PostMapping(value = "/store-category", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category) {
+    @PostMapping(value = "/store-category", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Category> createCategory(@Valid @RequestBody Category category) {
         categoryService.createCategory(category);
-        return ResponseEntity.ok("Category created successfully");
+        return ResponseEntity.ok(category);
     }
+
+
     @GetMapping("/category-list")
-    public ResponseEntity<List<Category>> getCategories(){
-    	List<Category> categories = categoryService.getAllCategories();
-    	return ResponseEntity.ok(categories);
+    public ResponseEntity<List<Category>> getCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        return ResponseEntity.ok(categories);
+    }
+
+    @PutMapping(value = "/category/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Category> updateCategory(@PathVariable Integer id, @Valid @RequestBody Category updatedCategory) {
+        boolean isUpdated = categoryService.updateCategory(id, updatedCategory);
+        if (isUpdated) {
+            Category updatedCategory1 = categoryService.getCategoryById(id);
+            if (updatedCategory1 != null) {
+                return ResponseEntity.ok(updatedCategory1);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/category-delete/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Integer id) {
+        boolean isDeleted = categoryService.deleteCategory(id);
+        if (isDeleted) {
+            return ResponseEntity.ok("Category deleted successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
 
